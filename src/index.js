e9766c76b9178f23dae4fea0c2025eb1d0db2068
angelacuahutle <angela.cuahutle@gmail.com>
2021-12-02 16:54:36 -0600

    Fix some linters

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const button = document.querySelector('button')
+const button = document.querySelector('button');
 const listArr = [{
-    index: 0,
-    duties: 'Finish repo',
-    completed: false
-  },
-  {
-    index: 1,
-    duties: 'Study session',
-    completed: false
-  },
-  {
-    index: 2,
-    duties: 'Meet partner',
-    completed: true
-  },
+  index: 0,
+  duties: 'Finish repo',
+  completed: false,
+},
+{
+  index: 1,
+  duties: 'Study session',
+  completed: false,
+},
+{
+  index: 2,
+  duties: 'Meet partner',
+  completed: true,
+},
 ];
 
 function displaylistArr() {
-  listArr.forEach((itemElement) => { 
-    const li = document.createElement('li')
-    li.innerHTML = `<li class= "list-group-item">${itemElement.duties}</li>`
-    button.parentElement.insertBefore(li,button);
-  })
+  listArr.forEach((itemElement) => {
+    const li = document.createElement('li');
+    li.innerHTML = `<li class= "list-group-item">${itemElement.duties}</li>`;
+    button.parentElement.insertBefore(li, button);
+  });
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-displaylistArr();
+  displaylistArr();
 });
