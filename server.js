0ec6512eebc76af9182ca51800e5ceaee5729870
angelacuahutle <angela.cuahutle@gmail.com>
2021-12-02 16:54:36 -0600

    Fix some linters

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
 const config = require('./webpack.config.js');
+
 const compiler = webpack(config);
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
@@ -11,10 +12,10 @@ const compiler = webpack(config);
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
-  })
+  }),
 );
 
 // Serve the files on port 5000.
-app.listen(5000, function () {
+app.listen(5000, () => {
   console.log('Example app listening on port 3000!\n');
 });
